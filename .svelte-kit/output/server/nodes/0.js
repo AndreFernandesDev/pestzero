752353620b3cf0f8fb25aa74a98e8207b76733be
AndreFernandesDev <andre.fernandesdev@gmail.com>
2022-06-27 16:47:35 +0100

    netlify deployment

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/fallbacks/layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const entry = 'layout.svelte-576b3812.js';
-export const js = ["layout.svelte-576b3812.js","chunks/index-7329c391.js"];
-export const css = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const entry = 'pages/index.svelte-c1ff4760.js';
-export const js = ["pages/index.svelte-c1ff4760.js","chunks/index-7329c391.js"];
-export const css = ["assets/pages/index.svelte-b0d2689d.css"];
